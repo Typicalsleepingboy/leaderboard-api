daa36fa4078121b51d571f27443a7b9e6cd04000
typicalsleepingboy <mpluto772@gmail.com>
2024-10-01 21:51:30 +0700

    fix

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const leaderboardRoute = require('./route/leaderboard');
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send({
+      message: "Lu mau nyari apa?? mending join discord intens aja",
+      author: "https://github.com/typicalsleepingboy",
+      discord: "https://discord.gg/48intenscommunity",
+    });
+  });
+  
 
 app.get('/admin/intens/leaderboard', leaderboardRoute);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
